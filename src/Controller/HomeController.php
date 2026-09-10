3e7faaaf927abffcce5788f86711dd73c691ac64
aintreallydown <viepro.db@gmail.com>
2026-09-11 00:42:40 +0200

    feat: Update layout and add new gallery and error pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -215,6 +215,13 @@ final class HomeController extends AbstractController
     {
         return $this->render('home/vision.html.twig');
     }
+    // #[Route('/gallerie', name: 'gallerie')]
+    // public function gallerie(): Response
+    // {
+    //     return $this->render('home/gallery.html.twig');
+    // }
+
+
 
     #[Route('/adhesion/reglement-interieur', name: 'reglement')]
     public function reglement(): Response
